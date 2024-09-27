db3eed010e7387bd35d6a9657f3546dac3b39d54
boltragons <pedrobotelho15@alu.ufc.br>
2024-09-27 18:38:15 -0300

    Added TC

---
+++ toolchain/aarch64-rpi4-linux-gnu/lib/gcc/aarch64-rpi4-linux-gnu/14.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/boltragons/rpi4/crosstool-ng/.build/aarch64-rpi4-linux-gnu/src/gcc/configure --build=x86_64-build_pc-linux-gnu --host=x86_64-build_pc-linux-gnu --target=aarch64-rpi4-linux-gnu --prefix=/home/boltragons/x-tools/aarch64-rpi4-linux-gnu --exec_prefix=/home/boltragons/x-tools/aarch64-rpi4-linux-gnu --with-sysroot=/home/boltragons/x-tools/aarch64-rpi4-linux-gnu/aarch64-rpi4-linux-gnu/sysroot --enable-languages=c,c++ --with-cpu=cortex-a72 --with-pkgversion='crosstool-NG 1.26.0.115_efcfd1a' --enable-__cxa_atexit --disable-libmudflap --disable-libgomp --disable-libssp --disable-libquadmath --disable-libquadmath-support --disable-libsanitizer --disable-libmpx --with-gmp=/home/boltragons/rpi4/crosstool-ng/.build/aarch64-rpi4-linux-gnu/buildtools --with-mpfr=/home/boltragons/rpi4/crosstool-ng/.build/aarch64-rpi4-linux-gnu/buildtools --with-mpc=/home/boltragons/rpi4/crosstool-ng/.build/aarch64-rpi4-linux-gnu/buildtools --with-isl=/home/boltragons/rpi4/crosstool-ng/.build/aarch64-rpi4-linux-gnu/buildtools --enable-lto --enable-threads=posix --enable-target-optspace --enable-plugin --enable-gold --disable-nls --disable-multilib --with-local-prefix=/home/boltragons/x-tools/aarch64-rpi4-linux-gnu/aarch64-rpi4-linux-gnu/sysroot --enable-long-long";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "cortex-a72" } };
